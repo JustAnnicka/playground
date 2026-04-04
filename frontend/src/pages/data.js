10d49b3dd87da71cdfcaf205a79ddbe049a5f328
aehrl <aehrl@student.42malaga.com>
2026-04-04 20:26:03 +0200

    Created a cards component

---
+++ frontend/src/pages/data.js
@@ -0,0 +1,9 @@
+ const DataPolicy = ()=> {
+
+	return (
+		<div>
+			<h1>Data privacy</h1>
+		</div>
+	)
+}
+export default DataPolicy
---
+++ frontend/src/pages/imprint.jsx
@@ -0,0 +1,10 @@
+
+const Imprint = () => { 
+	return ( 
+		<div className="Imprint"> 
+			<h1>Imprint</h1> 
+		</div> 
+	) 
+} 
+
+export default Imprint;
